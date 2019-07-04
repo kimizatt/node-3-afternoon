6184a978c03ec952fcdb1e475d291cec81572af9
Kimberly Izatt <kimizatt@Kims-MacBook-Pro.local>
2019-07-04 15:12:43 -0600

    Finished project

---
+++ product_controller.js
@@ -0,0 +1,56 @@
+module.exports = {
+    create: (req, res) => {
+        const db = req.app.get('db')
+        const {name, description, price, image_url } = req.body
+        
+        db.create_product([name, description, price, image_url])
+        .then( () => res.sendStatus(200))
+        .catch (err => {
+            res.status(500).send({errorMessage: "Oops! Something went wrong."})
+            console.log(err)
+        })    
+    }, 
+
+    getOne:(req, res) => {
+        const db = req.app.get('db')
+        const {id} = req.params
+        db.read_product(id)
+        .then(product => res.status(200).send(product))
+        .catch( err => {
+            res.status(500).send({errorMessage: "Oops! Something went wrong."})
+            console.log(err)
+        })
+    },
+
+    getAll:(req, res) => {
+        const db = req.app.get('db')
+        db.read_products()
+        .then(products => res.status(200).send(products))
+        .catch(err => {
+            res.status(500).send({errorMessage: 'Oops! Something went wrong.'})
+        })
+    }, 
+    
+    update: (req, res) => {
+        const db = req.app.get('db')
+        const {params, query} = req
+        db.update_product([params.id, query.desc])
+            .then((dbRes) => res.status(200).send(dbRes))
+            .catch(err => {
+                res.status(500).send({errorMessage: 'Oops!  Something went wrong.'})
+                console.log(err)
+            })
+        
+    } ,
+
+    delete: (req, res) => {
+        const db = req.app.get('db')
+        const {id} = req.params
+        db.delete_product(id) 
+        .then ( () => res.sendStatus(200))
+        .catch (err => {
+            res.status(500).send({errorMessage: 'Oops! Something went wrong.'})
+            console.log(err)
+        })
+    }
+}
